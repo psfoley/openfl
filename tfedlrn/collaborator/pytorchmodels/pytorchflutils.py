9fd0af5fd77eb55de1e414c41f991cd171a0eb01
Edwards, Brandon <brandon.edwards@intel.com>
2019-07-11 23:12:22 -0700

    Changes up to review with Micah.
    Change-Id: I86c9abdeab883fd6d1c50a7106feed33317a4429

--- tfedlrn/collaborator/pytorchmodels/pytorchflutils.py
+++ tfedlrn/collaborator/pytorchmodels/pytorchflutils.py
@@ -5,60 +5,87 @@ from copy import deepcopy
 
 def _derive_opt_state_dict(opt_state_dict):
     # Flattens the optimizer state dict so as to have key, value pairs with values as numpy arrays.
-    # The keys have sufficient information to know if the array becomes an int (meaning it was a 'step' value),
-    # or if it should be converted to torch.Tensor, or if the key is: '__opt_group_lengths' in which case the
-    # value is a 1-d array with components telling us the lengths of each parameter group.
-    # FIXME: Should I look into when to delete the opt_state_dict?
+    # The keys have sufficient info to restore opt_state_dict using expand_derived_opt_state_dict.
 
     derived_opt_state_dict = {}
 
+    # Determine if state is needed for this optimizer.
+    if len(opt_state_dict['state']) == 0:
+        derived_opt_state_dict['__opt_state_needed'] = 'false'
+        return derived_opt_state_dict
+
+    derived_opt_state_dict['__opt_state_needed'] = 'true'
+
+    # We assume that the state collected for all parameter groups is the same.
+    # Using one example state key, we collect keys for the corresponding dictionary value.
+    example_state_key = opt_state_dict['param_groups'][0]['params'][0]
+    state_subkeys = list(opt_state_dict['state'][example_state_key].keys())
+
+    # Tags will record whether the value associated to the subkey is a tensor or not.
+    state_subkey_tags = []
+    for state_subkey in state_subkeys:
+        if isinstance(opt_state_dict['state'][example_state_key][state_subkey], torch.Tensor):
+            state_subkey_tags.append('istensor')
+        else:
+            state_subkey_tags.append('')
+    state_subkeys_and_tags = list(zip(state_subkeys, state_subkey_tags))
+    
+    # Forming the flattened dict, using a concatenation of group index, subindex, tag,
+    # and subkey inserted into the flattened dict key - needed for reconstruction.
     nb_params_per_group = []
     for group_idx, group in enumerate(opt_state_dict['param_groups']):
         for idx, param_id in enumerate(group['params']):
-            for k, v in opt_state_dict['state'][param_id]:
-                if isinstance(v, torch.Tensor):
-                    derived_opt_state_dict['__opt_state_{}_{}_istensor_{}'.format(group_idx, idx, k)] = v.cpu().numpy()
+            for subkey, tag in state_subkeys_and_tags:
+                if tag == 'istensor':
+                    new_v = opt_state_dict['state'][param_id][subkey].cpu().numpy()
                 else:
-                    derived_opt_state_dict['__opt_state_{}_{}_{}'.format(group_idx, idx, k)] = np.array([v])
+                    new_v = np.array([opt_state_dict['state'][param_id][subkey]])
+                derived_opt_state_dict['__opt_state_{}_{}_{}_{}'.format(group_idx, idx, tag, subkey)] = new_v
         nb_params_per_group.append(idx + 1)
-    derived_opt_state_dict['__opt_group_shape'] = np.array(nb_params_per_group)
+    # group lengths are also helpful for reconstructing original opt_state_dict structure
+    derived_opt_state_dict['__opt_group_lengths'] = np.array(nb_params_per_group)
 
     return derived_opt_state_dict
 
 
 def expand_derived_opt_state_dict(derived_opt_state_dict, device):
     # Performs the inverse operations of _encode_and_flatten.
-    # FIXME: I am spending a lot of time searching for the right keys here, should I add more
-    #        structure? (will pay in message length?)
+
+    # Reconstructing state_subkeys_and_tags using the example key 
+    # prefix, "__opt_state_0_0_", certain to be present.
+    state_subkeys_and_tags = []
+    for key in derived_opt_state_dict:
+        if key.startswith('__opt_state_0_0_'):
+            stripped_key = key[16:]
+            if stripped_key.startswith('istensor_'):
+                this_tag = 'istensor'
+                subkey = stripped_key[9:]
+            else:
+                this_tag = ''
+                subkey = stripped_key[1:]
+            state_subkeys_and_tags.append((subkey, this_tag))
 
     opt_state_dict = {'param_groups': [], 'state': {}}
-    nb_params_per_group = list(opt_state_dict.pop('__opt_group_lengths').astype(np.int))
+    nb_params_per_group = list(derived_opt_state_dict.pop('__opt_group_lengths').astype(np.int))
 
+    # Construct the expanded dict.
     for group_idx, nb_params in enumerate(nb_params_per_group):
         these_group_ids = ['{}_{}'.format(group_idx, idx) for idx in range(nb_params)]
         opt_state_dict['param_groups'].append({'params': these_group_ids})
-        for id in these_group_ids:
-            opt_state_dict['state'][id] = {}
+        for this_id in these_group_ids:
+            opt_state_dict['state'][this_id] = {}
+            for subkey, tag in state_subkeys_and_tags:
+                flat_key = '__opt_state_{}_{}_{}'.format(this_id, tag, subkey)
+                if tag == 'istensor':
+                    new_v = torch.from_numpy(derived_opt_state_dict.pop(flat_key))
+                else:
+                    # Here (for currrently supported optimizers) the subkey should be 'step' 
+                    # and the length of array should be one.
+                    assert subkey == 'step'
+                    assert len(derived_opt_state_dict[flat_key]) == 1
+                    new_v = int(derived_opt_state_dict.pop(flat_key))
+                opt_state_dict['state'][this_id][subkey] = new_v 
 
-            # Here is the search referred to in the FIXME above
-            for k in derived_opt_state_dict:
-                target_string = '__opt_state_{}_'.format(id)
-
-                if k.startswith(target_string):
-                    # strip off the decoration on the key
-                    nb_to_strip = len(target_string)
-                    stripped_key = k[nb_to_strip:]
-                    # determine if the array should become a tensor
-                    is_tensor = stripped_key.startswith('istensor_')
-
-                    if is_tensor:
-                        opt_state_dict['state'][id][stripped_key[9:]] = \
-                            torch.Tensor(derived_opt_state_dict.pop(k)).to(device)
-                    else:
-                        # here the stripped key should be 'step' and the length of array should be one.
-                        assert stripped_key == 'step'
-                        assert len(derived_opt_state_dict['k']) == 1
-                        opt_state_dict['state'][id][stripped_key] = int(derived_opt_state_dict.pop(k))
 
     # sanity check that we did not miss any optimizer state
     assert len(derived_opt_state_dict) == 0
@@ -77,44 +104,54 @@ def _get_optimizer_state(optimizer):
 def _set_optimizer_state(optimizer, device, derived_opt_state_dict):
 
     temp_state_dict = expand_derived_opt_state_dict(derived_opt_state_dict, device)
+
+    # Setting default values.
+    # All optimizer.defaults are considered as not changing over course of training.
+    for group in temp_state_dict['param_groups']:
+        for k, v in optimizer.defaults.items():
+            group[k] = v
+
     optimizer.load_state_dict(temp_state_dict)
 
 
-def pt_get_tensor_dict(torch_nn, torch_optimizer):
-    # FIXME: should we use self.parameters()??? Unclear if load_state_dict() is better or simple assignment is better
-    # for now, state dict gives us names, which is good
 
+def pt_get_tensor_dict(torch_nn, torch_optimizer):
+    # FIXME: self.parameters() instead? Unclear if load_state_dict() or simple assignment is better
+    # for now, state dict gives us names which is good
     # FIXME: do both and sanity check each time?
 
-    # FIXME: can this have values other than the tensors????
-    state = torch_nn.state_dict()
+    # deep copy so as to decouple from active model
+    state = deepcopy(torch_nn.state_dict())
+
     for k, v in state.items():
-        state[k] = v.cpu().numpy()  # get as a numpy array
+        # When restoring, we currently assume all values are tensors.
+        assert torch.is_tensor(v)
+        state[k] = v.cpu().numpy()  # get as a numpy array, making sure is on cpu
 
     return {**state, **_get_optimizer_state(torch_optimizer)}
 
 
 def pt_set_tensor_dict(torch_nn, tensor_dict):
-    # FIXME: should we use self.parameters()??? Unclear if load_state_dict() is better or simple assignment is better
+    # FIXME: self.parameters() instead? Unclear if load_state_dict() or simple assignment is better
     # for now, state dict gives us names, which is good
-    
     # FIXME: do both and sanity check each time?
 
-    # get the model state and device so that we can determine the correct tensor values/device placements
-    model_state = torch_nn.state_dict()
+    # get device for correct placement of tensors
     device = torch_nn.device
 
     new_state = {}
-    for k, v in model_state.items():
-        new_state[k] = torch.Tensor(tensor_dict.pop(k)).to(device)
+    # Grabbing keys from model's state_dict helps to confirm we have everything
+    for k in torch_nn.state_dict():
+        new_state[k] = torch.from_numpy(tensor_dict.pop(k)).to(device)
 
     # set model state
     torch_nn.load_state_dict(new_state)
 
-    # next we have the optimizer state
-    _set_optimizer_state(torch_nn.get_optimizer(), device, tensor_dict)
+    # next we have the optimizer state, if there is state to restore
+    if tensor_dict.pop('__opt_state_needed') == 'true':
+        _set_optimizer_state(torch_nn.get_optimizer(), device, tensor_dict)
 
-    # sanity check that we did not miss anything
+    # sanity check that we did not record any state that was not used
     assert len(tensor_dict) == 0
 
 
@@ -147,7 +184,7 @@ def pt_train_epoch(torch_nn, train_loader, device, optimizer, loss_fn):
         loss.backward()
         optimizer.step()
         losses.append(loss.detach().cpu().numpy())
-        
+
     return np.mean(losses)
 
 

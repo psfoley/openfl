da6a2086dfd7d6f0ab9fac7561d80380fb3c4bdd
Weilin Xu <weilin.xu@intel.com>
2019-10-24 16:05:30 -0700

    Configured docstring auto generation.

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,10 +28,14 @@ author = 'Secure Intelligence Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import sphinx_rtd_theme
+import sphinxcontrib.napoleon
 
 extensions = [
     "sphinx_rtd_theme",
+    "sphinxcontrib.napoleon",
 ]
+
+napoleon_google_docstring = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

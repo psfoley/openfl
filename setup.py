c3ad01783ee399b03d91b4e74ddf044330ce3569
Weilin Xu <weilin.xu@intel.com>
2019-12-11 11:17:52 -0800

    Moved the dead model code to models/legacy.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='tfedlrn',
       version='0.0.0',
       package_dir={'tfedlrn': 'tfedlrn'},
-      packages=['tfedlrn', 'tfedlrn.aggregator', 'tfedlrn.collaborator', 'tfedlrn.coordinator', 'tfedlrn.proto', 'tfedlrn.collaborator.tensorflowmodels', 'tfedlrn.collaborator.pytorchmodels'],
+      packages=['tfedlrn', 'tfedlrn.aggregator', 'tfedlrn.collaborator', 'tfedlrn.coordinator', 'tfedlrn.proto'],
       # 'tfedlrn.bratsunettest', 
       install_requires=['protobuf', 'coloredlogs', 'pyyaml', 'nibabel', 'tensorboardX', 'grpcio']
 )

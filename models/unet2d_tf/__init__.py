e804a782bd649ee35c7e4e911e8de4a26c2a3068
Edwards <edwardsb@spr-gpu01.jf.intel.com>
2020-03-26 12:06:51 -0700

    Removed old model and data files.

--- models/unet2d_tf/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (C) 2020 Intel Corporation
-# Licensed subject to Collaboration Agreement dated February 28th, 2020 between Intel Corporation and Trustees of the University of Pennsylvania.
-
-

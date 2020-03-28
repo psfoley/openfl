4442e5108ee933f737be7fec3f5c054df7bb05f6
Hans <shih-han.wang@intel.com>
2020-03-27 17:10:35 -0700

    add tf data object and fix tf models __init__ and the setup.py

--- data/tensorflow/tfcifar10_inmemory.py
+++ data/tensorflow/tfcifar10_inmemory.py
@@ -3,26 +3,24 @@
 
 import numpy as np
 
+from data.tensorflow.tffldata_inmemory import TensorFlowFLDataInMemory 
 from data import load_cifar10_shard
-from data.tensorflow.tffldata_inmemory import TensorFlowFLDataInMemory
-
-
 
 class TensorFlowCIFAR10InMemory(TensorFlowFLDataInMemory):
 
     def __init__(self, data_path, batch_size, **kwargs):
+        """
+        Instantiate the data object 
 
+        Returns
+        -------
+        None
+        """
         super().__init__(batch_size)
 
         _, num_classes, X_train, y_train, X_val, y_val = load_cifar10_shard(shard_num=data_path, **kwargs)
-        
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_val = X_val
-        self.y_val = y_val
 
         self.num_classes = num_classes
-        
-
-
-        
+        self.batch_size = batch_size
+        self.train_loader = self.create_loader(X=X_train, y=y_train)
+        self.val_loader = self.create_loader(X=X_val, y=y_val)

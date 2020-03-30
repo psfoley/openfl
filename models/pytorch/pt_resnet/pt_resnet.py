5a67acf717319b8f0a2623e5b5f3d76b6cc8e8ea
Hans <shih-han.wang@intel.com>
2020-03-30 12:16:06 -0700

    update resnet18

--- models/pytorch/pt_resnet/pt_resnet.py
+++ models/pytorch/pt_resnet/pt_resnet.py
@@ -16,13 +16,37 @@ from models.pytorch import PyTorchFLModel
 def cross_entropy(output, target):
     return F.binary_cross_entropy_with_logits(input=output, target=target)
 
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
 class PyTorchResnet(PyTorchFLModel):
 
     def __init__(self, data, device='cpu', num_classes=10, **kwargs):
         super().__init__(data, device=device)
 
         self.num_classes = num_classes
-        self.init_network(device, **kwargs)
+        self.init_network(device, BasicBlock, [2,2,2,2], **kwargs)# Resnet18
         self._init_optimizer()        
 
         #def loss_fn(output, target):
@@ -53,33 +77,33 @@ class PyTorchResnet(PyTorchFLModel):
         return train_loader, val_loader
         '''
 
-    def init_network(self, device):
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-        self.to(device) 
-        #
-        '''
-        '''
+    def init_network(self, device, block, num_blocks, num_classes=10):
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))                                                                                                                                                        
-        x = self.pool(F.relu(self.conv2(x)))                                                                                                                                                        
-        x = x.view(-1, 16 * 5 * 5)                                                                                                                                                                  
-        x = F.relu(self.fc1(x))                                                                                                                                                                     
-        x = F.relu(self.fc2(x))                                                                                                                                                                     
-        x = self.fc3(x) 
-        #
-        #x = F.relu(self.conv1(x))
-        #x = F.max_pool2d(x, 2, 2)
-        #x = F.relu(self.conv2(x))
-        #x = F.max_pool2d(x, 2, 2)
-        #x = x.view(-1, 4*4*50)
-        #x = F.relu(self.fc1(x))
-        #x = self.fc2(x)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        x = self.linear(out)
         return F.log_softmax(x, dim=1)
 
     def validate(self):

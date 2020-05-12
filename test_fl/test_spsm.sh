de68e9c733fe2414ae436a2c56b3beca51f74725
Hans <shih-han.wang@intel.com>
2020-05-12 16:35:58 -0700

    updated

--- test_fl/test_spsm.sh
+++ test_fl/test_spsm.sh
@@ -1,6 +1,5 @@
-
 echo "running ... single process single machine"
 echo "testing pytorch creating initial weights ..."
 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_cnn_withmnist.yaml
 echo "testing pytorch training ..."
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withmnist.yaml
+#../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withmnist.yaml

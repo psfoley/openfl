ca6e662c22ee37ca9384d6f7362105e1b812de25
Hans <shih-han.wang@intel.com>
2020-05-13 09:06:11 -0700

    updated script

--- test_fl/test_mpsm.sh
+++ test_fl/test_mpsm.sh
@@ -1,16 +1,21 @@
+
+echo "-------------------------------------------------------------"
 echo "======== pki creating certificates ......"
-../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2.yaml
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2.yaml
+echo "-------------------------------------------------------------"
 echo "======== creating init weights ......"
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
 
 echo "-------------------------------------------------------------"
 echo "======== aggregator ......"
 echo "-------------------------------------------------------------"
-../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2.yaml &
+time ../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2.yaml &
 echo "-------------------------------------------------------------"
 echo "======== collaborator 0 ......"
 echo "-------------------------------------------------------------"
-../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_0 &
+time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_0 &
 echo "-------------------------------------------------------------"
 echo "======== collaborator 1 ......"
 echo "-------------------------------------------------------------"

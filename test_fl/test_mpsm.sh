8f5225878027e48d135ce2b1e08b47bab296670c
Hans <shih-han.wang@intel.com>
2020-05-13 08:54:53 -0700

    updated test case

--- test_fl/test_mpsm.sh
+++ test_fl/test_mpsm.sh
@@ -1,18 +1,18 @@
-echo "======== pki ......"
+echo "======== pki creating certificates ......"
 ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2.yaml
-echo "======== init weights ......"
+echo "======== creating init weights ......"
 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
 
 echo "-------------------------------------------------------------"
-echo "======== agg ......"
+echo "======== aggregator ......"
 echo "-------------------------------------------------------------"
 ../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2.yaml &
 echo "-------------------------------------------------------------"
-echo "======== col0 ......"
+echo "======== collaborator 0 ......"
 echo "-------------------------------------------------------------"
 ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_0 &
 echo "-------------------------------------------------------------"
-echo "======== col1 ......"
+echo "======== collaborator 1 ......"
 echo "-------------------------------------------------------------"
 time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_1
 echo "======== Done! ${0}"

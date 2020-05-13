f580e00867e052813bab30bc59a6477de1d74e8d
Hans <shih-han.wang@intel.com>
2020-05-13 08:30:48 -0700

    added to time the command in the spsm cases

--- test_fl/test_mpsm.sh
+++ test_fl/test_mpsm.sh
@@ -3,9 +3,16 @@ echo "======== pki ......"
 echo "======== init weights ......"
 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
 
+echo "-------------------------------------------------------------"
 echo "======== agg ......"
+echo "-------------------------------------------------------------"
 ../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2.yaml &
+echo "-------------------------------------------------------------"
 echo "======== col0 ......"
+echo "-------------------------------------------------------------"
 ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_0 &
+echo "-------------------------------------------------------------"
 echo "======== col1 ......"
+echo "-------------------------------------------------------------"
 time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_1
+echo "======== Done! ${0}"
--- test_fl/test_spsm.sh
+++ test_fl/test_spsm.sh
@@ -1,21 +1,31 @@
 echo "running ... single process single machine"
 
-echo "======== testing pytorch creating initial weights ..."
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_cnn_withmnist.yaml
-echo "======== testing pytorch training ..."
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withmnist.yaml
+echo "-------------------------------------------------------------"
+echo "1======== mnist, pytorch cnn creating initial weights ..."
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_cnn_withmnist.yaml
+echo "======== mnist, pytorch cnn training ..."
+time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withmnist.yaml
 
-echo "======== testing keras creating initial weights ..."
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_cnn_withmnist.yaml
-echo "======== testing kears training ..."
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_cnn_withmnist.yaml
+echo "-------------------------------------------------------------"
+echo "2======== mnist, keras cnn creating initial weights ..."
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_cnn_withmnist.yaml
+echo "======== mnist, keras cnn training ..."
+time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_cnn_withmnist.yaml
 
-echo "======== testing pytorch resnet creating initial weights ..."
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_resnet_withmnist.yaml
-echo "======== testing pytorch resnet training ..."
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_resnet_withmnist.yaml
+echo "-------------------------------------------------------------"
+echo "3======== mnist, pytorch resnet creating initial weights ..."
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_resnet_withmnist.yaml
+echo "======== mnist, pytorch resnet training ..."
+time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_resnet_withmnist.yaml
 
-echo "======== testing keras resnet creating initial weights ..."
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_resnet_withmnist.yaml
-echo "======== testing kears resnet training ..."
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_resnet_withmnist.yaml
+echo "-------------------------------------------------------------"
+echo "4======== mnist, keras resnet creating initial weights ..."
+echo "-------------------------------------------------------------"
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_resnet_withmnist.yaml
+echo "======== mnist, keras resnet training ..."
+time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_resnet_withmnist.yaml
+
+echo "======== Done! ${0}"

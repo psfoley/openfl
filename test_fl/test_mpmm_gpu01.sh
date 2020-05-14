bb421486108b7216c8d988dba77519717a456be7
Hans <shih-han.wang@intel.com>
2020-05-14 10:01:35 -0700

    updated yaml

--- test_fl/test_mpmm_gpu01.sh
+++ test_fl/test_mpmm_gpu01.sh
@@ -2,19 +2,19 @@
 echo "-------------------------------------------------------------"                                          
 echo "======== pki creating certificates ......"                                                              
 echo "-------------------------------------------------------------"                                          
-time ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2.yaml                            
+time ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2_mpmm.yaml                            
 echo "-------------------------------------------------------------"  
 
 echo "-------------------------------------------------------------"
 echo "======== on gpu01 ......"
 echo "======== creating init weights ......"
 echo "-------------------------------------------------------------"
-time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2_mpmm.yaml
 
 echo "-------------------------------------------------------------"
 echo "======== aggregator ......"
 echo "-------------------------------------------------------------"
-time ../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2.yaml 
+time ../venv/bin/python3 ../bin/run_aggregator_from_flplan.py -p keras_cnn_mnist_2_mpmm.yaml 
 echo "-------------------------------------------------------------"
 echo "======== Done! on gpu01 ${0}"
 
--- test_fl/test_mpmm_gpu02.sh
+++ test_fl/test_mpmm_gpu02.sh
@@ -2,7 +2,7 @@
 echo "-------------------------------------------------------------"                                          
 echo "======== pki creating certificates ......"                                                              
 echo "-------------------------------------------------------------"                                          
-time ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2.yaml                            
+time ../venv/bin/python3 ../bin/create_pki_for_flplan.py -p keras_cnn_mnist_2_mpmm.yaml                            
 echo "-------------------------------------------------------------"  
 
 sleep 60
@@ -11,14 +11,14 @@ echo "-------------------------------------------------------------"
 echo "======== on gpu02 ......"
 echo "======== creating init weights ......"
 echo "-------------------------------------------------------------"
-time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2.yaml
+time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p keras_cnn_mnist_2_mpmm.yaml
 
 echo "-------------------------------------------------------------"
 echo "======== collaborator 0 ......"
 echo "-------------------------------------------------------------"
-time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_0 &
+time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2_mpmm.yaml -col col_0 &
 echo "-------------------------------------------------------------"
 echo "======== collaborator 1 ......"
 echo "-------------------------------------------------------------"
-time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2.yaml -col col_1
+time ../venv/bin/python3 ../bin/run_collaborator_from_flplan.py -p keras_cnn_mnist_2_mpmm.yaml -col col_1
 echo "======== Done! on gpu02 ${0}"

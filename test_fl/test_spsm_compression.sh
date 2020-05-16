5593b315f4562bb4107587f7b6c3f6d898e73605
Hans <shih-han.wang@intel.com>
2020-05-15 18:55:29 -0700

    updated for merging to master

--- test_fl/test_spsm_compression.sh
+++
@@ -1,9 +0,0 @@
-#trap "exit" INT TERM ERR
-#trap "kill 0" EXIT
-
-echo "running ... single process single machine"                                                                  
-echo "creating initial weights ..."                                                                               
-../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p stc_compression_pipeline_test_mnist_10.yaml
-echo "training ..."                                                                                               
-../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p stc_compression_pipeline_test_mnist_10.yaml
-

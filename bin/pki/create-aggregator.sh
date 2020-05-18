852bfb4827bd10d686cc6f4b71819d331f5205d3
Micah Sheller <micah.j.sheller@intel.com>
2020-05-18 12:39:55 -0700

    Updated pki scripts to use script directory and append agg/col role to file names and paths

--- bin/pki/create-aggregator.sh
+++ bin/pki/create-aggregator.sh
@@ -1,3 +1,7 @@
+# ensure we are in the pki directory
+cd $(dirname $0)
+
+# default common_name to hostname.domainname
 common_name=$(hostname).$(hostname -d)
 subject_alt_name=DNS:$common_name
 
@@ -5,7 +9,7 @@ while getopts ":c:s:i:" opt; do
   case $opt in
     c) common_name="$OPTARG"
     ;;
-    s) subject_alt_name=DNS:"$OPTARG"
+    s) subject_alt_name=$subject_alt_name,DNS:"$OPTARG"
     ;;
     i) subject_alt_name=$subject_alt_name,IP:$OPTARG
     ;;
@@ -19,6 +23,9 @@ echo $subject_alt_name
 SAN=$subject_alt_name openssl req -new -config config/server.conf -subj "/CN=$common_name" -out $common_name.csr -keyout $common_name.key
 openssl ca -config config/signing-ca.conf -batch -extensions server_ext -in $common_name.csr -out $common_name.crt
 
-mkdir -p $common_name
-mv $common_name.crt $common_name.key $common_name
+filename_base=agg_$common_name
+
+mkdir -p $filename_base
+mv $common_name.crt $filename_base/$filename_base.crt
+mv $common_name.key $filename_base/$filename_base.key
 rm $common_name.csr
--- bin/pki/create-collaborator.sh
+++ bin/pki/create-collaborator.sh
@@ -1,4 +1,7 @@
-common_name=$(hostname).$(hostname -d)
+# ensure we are in the pki directory
+cd $(dirname $0)
+
+# default common_name to hostname.domainnamecommon_name=$(hostname).$(hostname -d)
 subject_alt_name=DNS:$common_name
 
 while getopts ":c:s:i:" opt; do
@@ -19,6 +22,9 @@ echo $subject_alt_name
 SAN=$subject_alt_name openssl req -new -config config/client.conf -out $common_name.csr -keyout $common_name.key -subj "/CN=$common_name /WD=123456"
 openssl ca -config config/signing-ca.conf -batch -in $common_name.csr -out $common_name.crt
 
-mkdir -p $common_name
-mv $common_name.crt $common_name.key $common_name
+filename_base=col_$common_name
+
+mkdir -p $filename_base
+mv $common_name.crt $filename_base/$filename_base.crt
+mv $common_name.key $filename_base/$filename_base.key
 rm $common_name.csr

69c656ed6e232d9d5f50e22d1ec2f6ecf8748ccb
Michael Beale <mbeale@intel.com>
2020-05-28 11:36:22 -0700

    added check for valid FQDN and valid IP address

--- bin/pki/create-collaborator.sh
+++ bin/pki/create-collaborator.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+function valid_fqdn()
+{
+    local fqdn=$1
+    local stat=0
+    result=`echo $fqdn | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'`
+    if [[ -z "$result" ]]
+    then
+        stat=1
+    fi
+    return $stat
+}
+
 if [ "$#" -ne 1 ];
 then
     echo "Usage: create-collaborator FQDN"
+    exit 1;
+fi
+
+if valid_fqdn $1; 
+then 
+    echo "Valid FQDN";
+else 
+    echo "Invalid FQDN";
     exit 1;
 fi
 

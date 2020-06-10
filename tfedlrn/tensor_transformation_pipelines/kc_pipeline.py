e7c492bad376c42a64eb2f331262e1d0a84933d7
Hans <shih-han.wang@intel.com>
2020-06-10 10:10:49 -0700

    document kc_pipeline.py

--- tfedlrn/tensor_transformation_pipelines/kc_pipeline.py
+++ tfedlrn/tensor_transformation_pipelines/kc_pipeline.py
@@ -6,13 +6,18 @@ from sklearn import cluster
 from tfedlrn.tensor_transformation_pipelines import TransformationPipeline, Transformer
 
 class KmeansTransformer(Transformer):
+    """ A transformer class to quantize input data.
+    """
     def __init__(self, n_cluster=6):
         self.n_cluster = n_cluster
         return
 
     def forward(self, data, **kwargs):
-        '''
-        '''
+        """ Quantize data into n_cluster levels of values.
+        data: an numpy array from the model tensor_dict.
+        int_data: an numpy array being quantized.
+        metadata: dictionary to store a list meta information.
+        """
         metadata = {}
         metadata['int_list'] = list(data.shape)
         # clustering
@@ -25,18 +30,13 @@ class KmeansTransformer(Transformer):
         int_array, int2float_map = self._float_to_int(quant_array)
         metadata['int_to_float']  = int2float_map
         return int_array, metadata
-        '''
-        # ternarization, data is sparse and flattened
-        mean_topk = np.mean(np.abs(data))
-        out_ = np.where(data > 0.0, mean_topk, 0.0)
-        out = np.where(data < 0.0, -mean_topk, out_)
-        int_array, int2float_map = self._float_to_int(out)
-        metadata = {}
-        metadata['int_to_float']  = int2float_map
-        return int_array, metadata
-        '''
 
     def backward(self, data, metadata, **kwargs):
+        """ Recover data array back to the original numerical type and the shape.
+        data: an flattened numpy array.
+        metadata: dictionary to contain information for recovering ack to original data array.
+        data (return): an numpy array with original numerical type and shape.
+        """
         # convert back to float
         # TODO
         data = copy.deepcopy(data)
@@ -49,6 +49,8 @@ class KmeansTransformer(Transformer):
         return data
 
     def _float_to_int(self, np_array):
+        """ Creating look-up table for conversion between floating and integer types.
+        """
         flatten_array = np_array.reshape(-1)
         unique_value_array = np.unique(flatten_array)
         int_array = np.zeros(flatten_array.shape, dtype=np.int)
@@ -64,39 +66,34 @@ class KmeansTransformer(Transformer):
         int_array = int_array.reshape(np_array.shape)
         return int_array, int_to_float_map
             
-    def _int_to_float(self, np_array, int_to_float_map):
-        flatten_array = np_array.reshape(-1)
-        unique_value_array = np.unique(flatten_array)
-        float_array = np.zeros(flatten_array.shape, dtype=np.int)
-        # create table
-        for idx, int_value in enumerate(unique_value_array):
-            float_value = int_to_float_map(int_value)
-            indices = np.where(np_array==int_value)
-            float_array[indices] = float_value
-        float_array = float_array.reshape(np_array.shape)
-        return int_array, int_to_float_map
-
 class GZIPTransformer(Transformer):
-    '''
-    '''
+    """ A transformer class to losslessly compress data.
+    """
     def __init__(self):
         return
 
     def forward(self, data, **kwargs):
+        """ Compress data into bytes.
+        """
         bytes_ = data.astype(np.float32).tobytes()
         compressed_bytes_ = gzip.compress(bytes_)
-        #shape_info = data.shape
         metadata = {}
         return compressed_bytes_, metadata
 
     def backward(self, data, metadata, **kwargs):
+        """ Decompress data into numpy of float32.
+        """
         decompressed_bytes_ = gzip.decompress(data)
         data = np.frombuffer(decompressed_bytes_, dtype=np.float32)
         return data
 
 class KCPipeline(TransformationPipeline):
+    """ A pipeline class to compress data lossly using k-means methods.
+    """
     
     def __init__(self, p_sparsity=0.01, n_clusters=6, **kwargs):
+        """ Initializing a pipeline of transformers.
+        """
         # instantiate each transformer
         self.p = p_sparsity
         self.n_cluster = n_clusters

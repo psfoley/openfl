be09159b85c4079fe2743e1db1277e4bd1a232ee
Hans <shih-han.wang@intel.com>
2020-06-10 10:10:33 -0700

    document skc_pipeline.py

--- tfedlrn/tensor_transformation_pipelines/skc_pipeline.py
+++ tfedlrn/tensor_transformation_pipelines/skc_pipeline.py
@@ -6,24 +6,23 @@ from sklearn import cluster
 from tfedlrn.tensor_transformation_pipelines import TransformationPipeline, Transformer
 
 class SparsityTransformer(Transformer):
+    """ A transformer class to sparsify input data.
+    """
     
     def __init__(self, p=0.01):
+        """ 
+        p: sparsity ratio
+        """
         self.p = p
         return
 
     def forward(self, data, **kwargs):
-        """
-        Implement the data transformation.
-        returns: transformed_data, metadata
-
-        here data is an array value from a model tensor_dict
-        """
-        print('======================================')
+        """ Sparsify data and pass over only non-sparsified elements by reducing the array size.                                               
+        data: an numpy array from the model tensor_dict.
+        condensed_data: an numpy array being sparsified.
+        metadata: dictionary to store a list of meta informaiton.                                                                              
+        """ 
         self.p = 1
-        print('sparsity::', self.p)
-        print('raw data::', data)
-        print('raw data::', data.shape)
-        print('======================================')
         metadata = {}
         metadata['int_list'] = list(data.shape)
         # sparsification
@@ -38,23 +37,14 @@ class SparsityTransformer(Transformer):
         sparse_data[topk_indices] = topk 
         nonzero_element_bool_indices = sparse_data != 0.0
         metadata['bool_list'] = list(nonzero_element_bool_indices)
-        print('======================================')
-        print('forward::')
-        print('condensed_data::', condensed_data)
-        print('======================================')
         return condensed_data, metadata
-        #return sparse_data, metadata
 
     def backward(self, data, metadata, **kwargs):
-        """
-        Implement the data transformation needed when going the oppposite
-        direction to the forward method.
-        returns: transformed_data
-        """
-        print('======================================')
-        print('backward::')
-        print('condensed_data::', data)
-        print('======================================')
+        """ Recover data array with the right shape and numerical type.
+        data: an numpy array with non-zero values.
+        metadata: dictionary to contain information for recovering back to original data array.
+        recovered_data: an numpy array with original shape.
+        """ 
         data = data.astype(np.float32)
         data_shape = metadata['int_list']
         nonzero_element_bool_indices = list(metadata['bool_list'])
@@ -65,6 +55,12 @@ class SparsityTransformer(Transformer):
         
 
     def _topk_func(self, x, k):
+        """ Select top k values.                                                                                                               
+        x: an numpy array to be sorted out for top-k components.                                                                               
+        k: k most maximum values.
+        topk_mag: components with top-k values.                                                                                                
+        indices: indices of the top-k components.                                                                                              
+        """
         # quick sort as default on magnitude
         idx = np.argsort(np.abs(x))
         # sorted order, the right most is the largest magnitude
@@ -78,17 +74,20 @@ class SparsityTransformer(Transformer):
         return topk_mag, indices
 
 class KmeansTransformer(Transformer):
+    """ A transformer class to quantize input data.
+    """ 
     def __init__(self, n_cluster=6):
         self.n_cluster = n_cluster
         return
 
     def forward(self, data, **kwargs):
-        '''
-        '''
+        """ Quantize data into n_cluster levels of values.
+        data: an flattened numpy array. 
+        int_data: an numpy array being quantized.
+        metadata: dictionary to store a list of meta information.
+        """
         # clustering
-        print('1data::', data.shape)
         data = data.reshape((-1,1))
-        print('2data::', data.shape)
         k_means = cluster.KMeans(n_clusters=self.n_cluster, n_init=self.n_cluster)
         k_means.fit(data)
         quantized_values = k_means.cluster_centers_.squeeze()
@@ -97,24 +96,16 @@ class KmeansTransformer(Transformer):
         int_array, int2float_map = self._float_to_int(quant_array)
         metadata = {}
         metadata['int_to_float']  = int2float_map
-        print('3int_array::', int_array.shape)
         int_array = int_array.reshape(-1)
-        print('4int_array::', int_array.shape)
         return int_array, metadata
-        '''
-        # ternarization, data is sparse and flattened
-        mean_topk = np.mean(np.abs(data))
-        out_ = np.where(data > 0.0, mean_topk, 0.0)
-        out = np.where(data < 0.0, -mean_topk, out_)
-        int_array, int2float_map = self._float_to_int(out)
-        metadata = {}
-        metadata['int_to_float']  = int2float_map
-        return int_array, metadata
-        '''
 
     def backward(self, data, metadata, **kwargs):
+        """ Recover data array back to the original numerical type.
+        data: an numpy array with non-zero values.
+        metadata: dictionary to contain information for recovering back to original data array.
+        data (return): an numpy array with original numerical type.
+        """
         # convert back to float
-        # TODO
         data = copy.deepcopy(data)
         int2float_map = metadata['int_to_float']
         for key in int2float_map:
@@ -123,6 +114,8 @@ class KmeansTransformer(Transformer):
         return data
 
     def _float_to_int(self, np_array):
+        """ Creating look-up table for conversion between floating and integer types.
+        """
         flatten_array = np_array.reshape(-1)
         unique_value_array = np.unique(flatten_array)
         int_array = np.zeros(flatten_array.shape, dtype=np.int)
@@ -138,39 +131,34 @@ class KmeansTransformer(Transformer):
         int_array = int_array.reshape(np_array.shape)
         return int_array, int_to_float_map
             
-    def _int_to_float(self, np_array, int_to_float_map):
-        flatten_array = np_array.reshape(-1)
-        unique_value_array = np.unique(flatten_array)
-        float_array = np.zeros(flatten_array.shape, dtype=np.int)
-        # create table
-        for idx, int_value in enumerate(unique_value_array):
-            float_value = int_to_float_map(int_value)
-            indices = np.where(np_array==int_value)
-            float_array[indices] = float_value
-        float_array = float_array.reshape(np_array.shape)
-        return int_array, int_to_float_map
-
 class GZIPTransformer(Transformer):
-    '''
-    '''
+    """ A transformer class to losslessly compress data.
+    """
     def __init__(self):
         return
 
     def forward(self, data, **kwargs):
+        """ Compress data into bytes.
+        """
         bytes_ = data.astype(np.float32).tobytes()
         compressed_bytes_ = gzip.compress(bytes_)
-        #shape_info = data.shape
         metadata = {}
         return compressed_bytes_, metadata
 
     def backward(self, data, metadata, **kwargs):
+        """ Decompress data into numpy of float32.
+        """
         decompressed_bytes_ = gzip.decompress(data)
         data = np.frombuffer(decompressed_bytes_, dtype=np.float32)
         return data
 
 class SKCPipeline(TransformationPipeline):
+    """ A pipeline class to compress data lossly using sparsity and k-means methods.
+    """
     
     def __init__(self, p_sparsity=0.01, n_clusters=6, **kwargs):
+        """ Initializing a pipeline of transformers. 
+        """
         # instantiate each transformer
         self.p = p_sparsity
         self.n_cluster = n_clusters

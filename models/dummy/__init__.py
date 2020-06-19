33167eb60cdc8874e23b0524e5fa6bccc46f79bf
Edwards <edwardsb@spr-gpu01.jf.intel.com>
2020-06-19 13:57:12 -0700

    removed imports into higher model module __init__s

--- models/dummy/__init__.py
+++ models/dummy/__init__.py
@@ -1,4 +1,3 @@
 # Copyright (C) 2020 Intel Corporation
 # Licensed subject to the terms of the separately executed evaluation license agreement between Intel Corporation and you.
 
-from .dummymodel import DummyModel
--- models/tensorflow/__init__.py
+++ models/tensorflow/__init__.py
@@ -3,6 +3,3 @@
 
 from .tfflmodel import TensorFlowFLModel
 from .kerasflmodel import KerasFLModel
-from .tf_2dunet.tf_2dunet import TensorFlow2DUNet
-from .keras_cnn.keras_cnn import KerasCNN
-from .keras_resnet.keras_resnet import KerasResnet

212b0c6371272ba4bd81e2fec24ee05bfc1e3189
Sheller, Micah J <micah.j.sheller@intel.com>
2020-06-19 16:56:43 -0700

    Script now finds a GPU with less than 1 MiB memory in use and runs on just that GPU
    Change-Id: Iae63bdd3202512158e379fdcbf28a0ed171d0037

--- test_fl/test_spsm_gpu.sh
+++ test_fl/test_spsm_gpu.sh
@@ -4,29 +4,49 @@ echo "running ... single process single machine"
 echo "-------------------------------------------------------------"
 echo "1======== cifar10, pytorch cnn creating initial weights ..."
 echo "-------------------------------------------------------------"
-time CUDA_VISIBLE_DEVICES=9 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_cnn_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_cnn_withcifar10.yaml
+
 echo "======= cifar10, pytorch cnn training ..."
-time CUDA_VISILBE_DEVICES=9 ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_cnn_withcifar10.yaml
 
 echo "-------------------------------------------------------------"
 echo "2======= cifar10, keras cnn creating initial weights ..."
 echo "-------------------------------------------------------------"
-time CUDA_VISIBLE_DEVICES=9 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_cnn_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_cnn_withcifar10.yaml
+
 echo "======== cifar10, keras cnn training ..."
-time CUDA_VISILBE_DEVICES=9 ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_cnn_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_cnn_withcifar10.yaml
 
 echo "-------------------------------------------------------------"
 echo "3======== cifar10, pytorch resnet creating initial weights ..."
 echo "-------------------------------------------------------------"
-time CUDA_VISIBLE_DEVICES=9 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_resnet_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p pt_resnet_withcifar10.yaml
+
 echo "======== cifar10, pytorch resent training ..."
-time CUDA_VISILBE_DEVICES=9 ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_resnet_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p pt_resnet_withcifar10.yaml
 
 echo "-------------------------------------------------------------"
 echo "4======= cifar10, keras resnet creating initial weights ..."
 echo "-------------------------------------------------------------"
-time CUDA_VISIBLE_DEVICES=9 ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_resnet_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/create_initial_weights_file_from_flplan.py -p tf_resnet_withcifar10.yaml
+
 echo "======= cifar10, keras resnet training ..."
-time CUDA_VISILBE_DEVICES=9 ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_resnet_withcifar10.yaml
+GPU=$(./find_free_gpu.sh)
+echo "Using GPU $GPU"
+CUDA_VISIBLE_DEVICES=$GPU time ../venv/bin/python3 ../bin/run_simulation_from_flplan.py -p tf_resnet_withcifar10.yaml
 
 echo "======== Done! ${0}"

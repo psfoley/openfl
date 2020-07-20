031266471eef822078a8d9dbd86f899f82a096a8
Sheller, Micah J <micah.j.sheller@intel.com>
2020-07-20 16:28:26 -0700

    Added script to display the full flplan
    Change-Id: Id30e8c62ffbc0a8010805490a98eccc5d79aae91

---
+++ bin/display_full_flplan.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020 Intel Corporation
+# Licensed subject to the terms of the separately executed evaluation license agreement between Intel Corporation and you.
+
+import argparse
+import sys
+import os
+import logging
+
+from tfedlrn import parse_fl_plan, get_object, load_yaml
+
+from setup_logging import setup_logging
+
+
+def main(plan, logging_config_fname, logging_default_level):
+    setup_logging(path=logging_config_fname, default_level=logging_default_level)
+
+    # FIXME: consistent filesystem (#15)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    base_dir = os.path.join(script_dir, 'federations')
+    plan_dir = os.path.join(base_dir, 'plans')
+
+    parse_fl_plan(os.path.join(plan_dir, plan))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--plan', '-p', type=str, required=True)
+    parser.add_argument('--logging_config_fname', '-lc', type=str, default="logging.yaml")
+    parser.add_argument('--logging_default_level', '-l', type=str, default="info")
+    args = parser.parse_args()
+    main(**vars(args))

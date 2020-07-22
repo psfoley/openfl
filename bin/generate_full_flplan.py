08cd6b89ae8e1cc55a3e18b7c570a32f57f74c97
Micah Sheller <micah.j.sheller@intel.com>
2020-07-22 10:21:37 -0700

    Renamed to 'generate_full_flplan.py'. No longer uses logger, so stdout is clean. Now accepts --out_filepath to write to.

--- bin/generate_full_flplan.py
+++ bin/generate_full_flplan.py
@@ -6,27 +6,30 @@
 import argparse
 import sys
 import os
-import logging
 
-from tfedlrn import parse_fl_plan, get_object, load_yaml
+import yaml
 
-from setup_logging import setup_logging
+from tfedlrn import parse_fl_plan
 
 
-def main(plan, logging_config_fname, logging_default_level):
-    setup_logging(path=logging_config_fname, default_level=logging_default_level)
+def main(plan, out_filepath):
 
     # FIXME: consistent filesystem (#15)
     script_dir = os.path.dirname(os.path.realpath(__file__))
     base_dir = os.path.join(script_dir, 'federations')
     plan_dir = os.path.join(base_dir, 'plans')
 
-    parse_fl_plan(os.path.join(plan_dir, plan))
+    flplan = yaml.dump(parse_fl_plan(os.path.join(plan_dir, plan)))
+
+    print(flplan)
+    if out_filepath is not None:
+        with open(out_filepath, 'w') as f:
+            f.write(flplan)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--plan', '-p', type=str, required=True)
-    parser.add_argument('--logging_config_fname', '-lc', type=str, default="logging.yaml")
-    parser.add_argument('--logging_default_level', '-l', type=str, default="info")
+    parser.add_argument('--out_filepath', '-o', type=str, default=None)
     args = parser.parse_args()
     main(**vars(args))

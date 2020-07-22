802673d67a5a682aab6229c5cce90c47b07b1e7a
Tony Reina <g.anthony.reina@intel.com>
2020-07-22 11:19:34 -0700

    Adding Sphinx documentation to Python files

--- bin/create_initial_weights_file_from_flplan.py
+++ bin/create_initial_weights_file_from_flplan.py
@@ -14,10 +14,38 @@ from tfedlrn.proto.protoutils import dump_proto, construct_proto
 from setup_logging import setup_logging
 
 def get_data(data_names_to_paths, data_name, module_name, class_name, **kwargs):
+    """Creates a data object from the data path
+
+    Args:
+        data_names_to_paths: Dictionary to convert data_name to file path
+        data_name: Name of the dataset to use from the data configuration file
+        module_name: Class module for the model
+        class_name: Model class name
+        **kwargs: Additional variable arguments to pass to the function
+
+    Returns:
+        A data object
+
+    """
     data_path = data_names_to_paths[data_name]
     return get_object(module_name, class_name, data_path=data_path, **kwargs)
 
 def main(plan, collaborators_file, feature_shape, data_config_fname, logging_config_path, logging_default_level):
+    """Creates a protobuf file of the initial weights for the model
+
+    Uses the federation (FL) plan to create an initial weights file
+    for the federation.
+
+    Args:
+        plan: The federation (FL) plan filename
+        collaborators_file:
+        feature_shape: The input shape to the model
+        data_config_fname: The data configuration file (defines where the datasets are located)
+        logging_config_path: The log path
+        logging_default_level (int): The default log level
+
+    """
+
     setup_logging(path=logging_config_path, default_level=logging_default_level)
 
     logger = logging.getLogger(__name__)
@@ -25,7 +53,7 @@ def main(plan, collaborators_file, feature_shape, data_config_fname, logging_con
     # FIXME: consistent filesystem (#15)
     script_dir = os.path.dirname(os.path.realpath(__file__))
     base_dir = os.path.join(script_dir, 'federations')
-    
+
     plan_dir = os.path.join(base_dir, 'plans')
     weights_dir = os.path.join(base_dir, 'weights')
     if not os.path.exists(weights_dir):
@@ -57,26 +85,26 @@ def main(plan, collaborators_file, feature_shape, data_config_fname, logging_con
         logger.info('Using data object of type {} and feature shape {}'.format(type(data), feature_shape))
 
     wrapped_model = get_object(data=data, **model_config)
-    
+
     fpath = os.path.join(weights_dir, agg_config['init_model_fname'])
 
     tensor_dict_split_fn_kwargs = wrapped_model.tensor_dict_split_fn_kwargs or {}
-    
-    tensor_dict, holdout_params = split_tensor_dict_for_holdouts(logger, 
-                                                                 wrapped_model.get_tensor_dict(False), 
+
+    tensor_dict, holdout_params = split_tensor_dict_for_holdouts(logger,
+                                                                 wrapped_model.get_tensor_dict(False),
                                                                  **tensor_dict_split_fn_kwargs)
     logger.warn('Following paramters omitted from global initial model, '\
                 'local initialization will determine values: {}'.format(list(holdout_params.keys())))
 
-    model_proto = construct_proto(tensor_dict=tensor_dict, 
-                                  model_id=wrapped_model.__class__.__name__, 
+    model_proto = construct_proto(tensor_dict=tensor_dict,
+                                  model_id=wrapped_model.__class__.__name__,
                                   model_version=0,
-                                  is_delta=False, 
-                                  delta_from_version=-1, 
+                                  is_delta=False,
+                                  delta_from_version=-1,
                                   compression_pipeline=compression_pipeline)
 
     dump_proto(model_proto=model_proto, fpath=fpath)
-    
+
     logger.info("Created initial weights file: {}".format(fpath))
 
 
--- bin/flplan_info_to_stdout.py
+++ bin/flplan_info_to_stdout.py
@@ -5,6 +5,15 @@ import yaml
 # this module is used to to parse flplans in preparation of building docker containers
 # here we have a copy of load_yaml from tfedlrn.utils (trying to avoid instalation of tfedlrn)
 def load_yaml(path):
+    """Parses the Federation Plan (FL plan) from the YAML file.
+
+    Args:
+        path: The directory path for the FL plan YAML file.
+
+    Returns:
+        A YAMLObject with the Federation plan (FL Plan)
+
+    """
     plan = None
     with open(path, 'r') as f:
         plan = yaml.safe_load(f.read())
@@ -12,7 +21,16 @@ def load_yaml(path):
 
 
 def get_until_none(key_list, _dict):
-    # return string version of info found in flplan when following key list until key==None or end of key_list
+    """Return string version of info found in federation (FL) plan when following key list until key==None or end of key_list
+
+    Args:
+        key_list: A list of keys from the dictionary
+        _dict: The dictionary to parse
+
+    Returns:
+        The string value of the dictionary for that key
+
+    """
     result = _dict
     for key in key_list:
         if key is None:
@@ -20,10 +38,10 @@ def get_until_none(key_list, _dict):
         else:
             result = result[key]
     return str(result)
-    
+
 
 def main(plan, key_list=[None]):
-    
+
     # FIXME: consistent filesystem (#15)
     script_dir = os.path.dirname(os.path.realpath(__file__))
     base_dir = os.path.join(script_dir, 'federations')
@@ -37,7 +55,7 @@ def main(plan, key_list=[None]):
     print(info)
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--plan', '-p', type=str, required=True)
     parser.add_argument('--key_list', '-kl', type=str, nargs="*")

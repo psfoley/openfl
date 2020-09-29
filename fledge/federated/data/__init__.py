8210aeb2194371cd5b5ec0a67c032404296f547a
Patrick Foley <patrick.foley@intel.com>
2020-09-29 11:19:47 -0700

    Conditionally import tensorflow, pytorch task runners and dataloader depending on whether they are installed. This fixes the error that could be thrown if trying to initialize a workspace in a fresh virtual environment. Also includes better error messages in the case that the user attempts to initialize their plan without installing the necessary frameworks

--- fledge/federated/data/__init__.py
+++ fledge/federated/data/__init__.py
@@ -2,12 +2,16 @@
 # Licensed subject to the terms of the separately executed evaluation license agreement between Intel Corporation and you.
 
 from warnings import catch_warnings, simplefilter
+import pkgutil
     
 with catch_warnings():
     simplefilter(action = 'ignore', category = FutureWarning)
-    import tensorflow # ignore deprecation warnings in command-line interface
+    if (True if pkgutil.find_loader('tensorflow') else False):
+      import tensorflow # ignore deprecation warnings in command-line interface
 
 from .loader       import           DataLoader
-from .loader_tf    import TensorFlowDataLoader
-from .loader_pt    import    PyTorchDataLoader
-from .loader_keras import      KerasDataLoader
+if (True if pkgutil.find_loader('tensorflow') else False):
+  from .loader_tf    import TensorFlowDataLoader
+  from .loader_keras import      KerasDataLoader
+if (True if pkgutil.find_loader('torch') else False):
+  from .loader_pt    import    PyTorchDataLoader

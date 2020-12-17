b118aafafb1b48506fd0595b30bbbee95a7d9872
Patrick Foley <patrick.foley@intel.com>
2020-12-16 16:53:41 -0800

    Replaced keras mnist example with pytorch

--- tests/gitlab/python_native_test.py
+++ tests/gitlab/python_native_test.py
@@ -1,30 +1,39 @@
-import mnist
+import numpy as np
+import torch
 import json
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.utils import to_categorical
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
+import torchvision
+import torchvision.transforms as transforms
 import fledge.native as fx
 from fledge.federated import FederatedModel, FederatedDataSet
 
-train_images = mnist.train_images()
-train_labels = to_categorical(mnist.train_labels())
-valid_images = mnist.test_images()
-valid_labels = to_categorical(mnist.test_labels())
+fx.init('torch_cnn_mnist')
 
 
-def preprocess(images):
-    # Normalize
-    images = (images / 255) - 0.5
-    # Flatten
-    images = images.reshape((-1, 784))
-    return images
+def one_hot(labels, classes):
+    return np.eye(classes)[labels]
 
 
-# Preprocess the images.
-train_images = preprocess(train_images)
-valid_images = preprocess(valid_images)
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                      download=True, transform=transform)
+
+train_images, train_labels = trainset.train_data, np.array(trainset.train_labels)
+train_images = torch.from_numpy(np.expand_dims(train_images, axis=1)).float()
+train_labels = one_hot(train_labels, 10)
+
+validset = torchvision.datasets.MNIST(root='./data', train=False,
+                                      download=True, transform=transform)
+
+valid_images, valid_labels = validset.test_data, np.array(validset.test_labels)
+valid_images = torch.from_numpy(np.expand_dims(valid_images, axis=1)).float()
+valid_labels = one_hot(valid_labels, 10)
 feature_shape = train_images.shape[1]
 classes = 10
 
@@ -32,19 +41,38 @@ fl_data = FederatedDataSet(train_images, train_labels, valid_images, valid_label
                            batch_size=32, num_classes=classes)
 
 
-def build_model(feature_shape, classes):
-    # Defines the MNIST model
-    model = Sequential()
-    model.add(Dense(64, input_shape=feature_shape, activation='relu'))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(classes, activation='softmax'))
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.fc1 = nn.Linear(32 * 5 * 5, 32)
+        self.fc2 = nn.Linear(32, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    return model
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
 
 
-fl_model = FederatedModel(build_model, data_loader=fl_data)
+def optimizer(x):
+    optim.Adam(x, lr=1e-4)
 
+
+def cross_entropy(output, target):
+    """Binary cross-entropy metric
+    """
+    return F.binary_cross_entropy_with_logits(input=output, target=target)
+
+
+fl_model = FederatedModel(build_model=Net, optimizer=optimizer,
+                          loss_fn=cross_entropy, data_loader=fl_data)
 collaborator_models = fl_model.setup(num_collaborators=2)
 collaborators = {'one': collaborator_models[0], 'two': collaborator_models[1]}
 print(f'Original training data size: {len(train_images)}')
@@ -63,7 +91,5 @@ print(f'Collaborator two\'s validation data size: \
         {len(collaborator_models[1].data_loader.X_valid)}\n')
 
 print(json.dumps(fx.get_plan(), indent=4, sort_keys=True))
-final_fl_model = fx.run_experiment(collaborators,
-                                   override_config={'aggregator.settings.rounds_to_train': 5})
-# Save final model
-final_fl_model.save_native('final_model')
+final_fl_model = fx.run_experiment(collaborators, {'aggregator.settings.rounds_to_train': 5})
+final_fl_model.save_native('final_pytorch_model')

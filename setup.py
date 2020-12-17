72e571f1ed7145618e18787da9231376d5d0cf18
Patrick Foley <patrick.foley@intel.com>
2020-12-17 14:47:48 -0800

    Update version to 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='fledge',
-    version='0.3.1',
+    version='0.3.2',
     author='Intel Corporation',
     description='Federated Learning on the Edge',
     packages=[

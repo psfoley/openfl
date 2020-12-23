b1210938c6c61a959016eaac5ee05fb7b7ad71b0
Trushkin, Ilya <ilya.trushkin@intel.com>
2020-12-23 13:54:04 +0300

    Rearrange fledge.federated imports

--- fledge/federated/__init__.py
+++ fledge/federated/__init__.py
@@ -8,12 +8,12 @@ from .plan import Plan  # NOQA
 from .task import TaskRunner  # NOQA
 from .data import DataLoader  # NOQA
 
-if pkgutil.find_loader('tensorflow'):
+if pkgutil.find_loader('torch') and pkgutil.find_loader('tensorflow'):
+    from .task import FastEstimatorTaskRunner, FederatedModel  # NOQA
+    from .data import FastEstimatorDataLoader, FederatedDataSet  # NOQA
+elif pkgutil.find_loader('tensorflow'):
     from .task import TensorFlowTaskRunner, KerasTaskRunner, FederatedModel  # NOQA
     from .data import TensorFlowDataLoader, KerasDataLoader, FederatedDataSet  # NOQA
-if pkgutil.find_loader('torch'):
-    from .task import PyTorchTaskRunner, FederatedModel  # NOQA
-    from .data import PyTorchDataLoader, FederatedDataSet  # NOQA
-if pkgutil.find_loader('torch') and pkgutil.find_loader('tensorflow'):
-    from .task import FastEstimatorTaskRunner  # NOQA
-    from .data import FastEstimatorDataLoader  # NOQA
+elif pkgutil.find_loader('torch'):
+    from .task import PyTorchTaskRunner, FederatedModel # NOQA
+    from .data import PyTorchDataLoader, FederatedDataSet # NOQA

588281afeb53a68f3b29e1b7ffa8e084880ee3cd
mansishr <77758170+mansishr@users.noreply.github.com>
2022-08-10 09:26:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Click',
-        'PyYAML==5.4.1',
+        'PyYAML>=5.4.1',
         'cloudpickle',
         'cryptography>=3.4.6',
         'docker',
